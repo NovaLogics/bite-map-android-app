979fb41ff1ba0c2bc5381b69def355e0bdd62d91
NovaLogics <rajitha.shavinda@gmail.com>
2025-02-20 00:38:28 +0530

    feat(location): update places-screen components

--- app/src/main/java/novalogics/android/bitemap/location/presentation/screens/places/PlacesSearch.kt
+++ app/src/main/java/novalogics/android/bitemap/location/presentation/screens/places/PlacesSearch.kt
@@ -1,23 +1,42 @@
 package novalogics.android.bitemap.location.presentation.screens.places
 
+import android.Manifest
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -25,106 +44,204 @@ import androidx.navigation.NavHostController
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.android.gms.maps.model.LatLng
+import novalogics.android.bitemap.R
+import novalogics.android.bitemap.app.ui.theme.BiteMapTheme
+import novalogics.android.bitemap.core.navigation.DashboardRoute
 import novalogics.android.bitemap.core.navigation.LocationRoute
 import novalogics.android.bitemap.core.navigation.events.PlacesResult
 import novalogics.android.bitemap.location.domain.model.PlaceDetails
 
-@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun RestaurantFinderScreen(
     navHostController: NavHostController,
     viewModel: PlacesSearchViewModel = hiltViewModel<PlacesSearchViewModel>(),
-    goToGoogleMap:(PlaceDetails) -> Unit
+    goToGoogleMap: (PlaceDetails) -> Unit
 ) {
-    val permission = rememberMultiplePermissionsState(
-        permissions = listOf(
-            android.Manifest.permission.ACCESS_COARSE_LOCATION,
-            android.Manifest.permission.ACCESS_FINE_LOCATION,
-        )
+    val uiState by viewModel.uiState.collectAsState()
+
+    PermissionHandler(navController = navHostController)
+
+    ScreenUiContent(
+        uiState = uiState,
+        navHostController = navHostController,
+        updateQuery = { viewModel.handleIntent(PlacesContract.Intent.UpdateQuery(it)) },
+        goToGoogleMap = goToGoogleMap,
+        fetchDetails = { id, result ->
+            viewModel.handleIntent(
+                PlacesContract.Intent.FetchDetails(
+                    placeId = id,
+                    result = result
+                )
+            )
+        }
     )
+}
 
-    val placesResult = viewModel.search.value
-    val query = viewModel.query.collectAsState()
-    if (permission.allPermissionsGranted) {
+@Composable
+fun ScreenUiContent(
+    uiState: PlacesContract.PlacesUiState,
+    navHostController: NavHostController,
+    updateQuery: (String) -> Unit,
+    goToGoogleMap: (PlaceDetails) -> Unit,
+    fetchDetails: (placeId: String, result: (PlaceDetails) -> Unit) -> Unit
+) {
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        val (searchCons, listCons) = createRefs()
 
-        ConstraintLayout(
+        TextField(
+            value = uiState.searchQuery,
+            onValueChange = { updateQuery(it) },
+            maxLines = 1,
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    tint = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.5F),
+                    contentDescription = stringResource(id = R.string.search_icon_description)
+                )
+            },
             modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background)
-        ) {
-            val (searchCons, listCons) = createRefs()
+                .fillMaxWidth()
+                .constrainAs(searchCons) {
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    top.linkTo(parent.top)
+                })
 
-            TextField(
-                value = query.value,
-                onValueChange = { viewModel.updateQuery(it) },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .constrainAs(searchCons) {
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                        top.linkTo(parent.top)
-                    })
-
-            when (placesResult) {
-                is PlacesResult.Idle -> {}
-                is PlacesResult.Loading -> {
-                    Box(
-                        modifier = Modifier.fillMaxSize(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        CircularProgressIndicator()
-                    }
-                }
-
-                is PlacesResult.Success -> {
-                    LazyColumn(modifier = Modifier.constrainAs(listCons) {
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                        top.linkTo(searchCons.bottom)
-                        bottom.linkTo(parent.bottom)
-                        height = Dimension.fillToConstraints
-                    }) {
-                        items(placesResult.list) {
-                            Text(
-                                text = it.getFullText(null).toString(),
-                                color = MaterialTheme.colorScheme.onPrimary,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(12.dp)
-                                    .clickable {
-                                        viewModel.fetchDetails(it.placeId){
-                                            it.origin = LatLng(
-                                                placesResult.location?.latitude!!,
-                                                placesResult.location?.longitude!!,
-                                            )
-                                            goToGoogleMap.invoke(it)
-                                            navHostController.navigate(LocationRoute.GOOGLE_MAPS.route)
-                                        }
-                                    }
-                            )
-                        }
-                    }
-                }
-
-                is PlacesResult.Error -> {
-                    Box(
-                        modifier = Modifier.fillMaxSize(),
-                        contentAlignment = Alignment.Center
-                    ) {
+        when (uiState.searchResult) {
+            is PlacesResult.Idle -> {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Column(
+                        modifier = Modifier,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        ) {
                         Text(
-                            text = placesResult.message.toString(),
-                            color = MaterialTheme.colorScheme.onError,
-                            modifier = Modifier.padding(24.dp)
+                            text = stringResource(id = R.string.search_restaurants),
+                            color = MaterialTheme.colorScheme.onSurface,
+                            style = MaterialTheme.typography.displayMedium.copy(
+                                fontWeight = FontWeight.Normal,
+                                fontSize = 24.sp,
+                            ),
+                            textAlign = TextAlign.Center,
+                            modifier =  Modifier.padding(12.dp)
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Image(
+                            painter = painterResource(id = R.drawable.bitemap_logo_main),
+                            contentDescription = "BiteMap Logo",
+                            modifier = Modifier
+                                .size(180.dp)
+                                .align(Alignment.CenterHorizontally)
                         )
                     }
                 }
             }
+            is PlacesResult.Loading -> {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
+
+            is PlacesResult.Success -> {
+                LazyColumn(modifier = Modifier.constrainAs(listCons) {
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    top.linkTo(searchCons.bottom)
+                    bottom.linkTo(parent.bottom)
+                    height = Dimension.fillToConstraints
+                }) {
+                    items(uiState.searchResult.list) { place->
+                        Text(
+                            text = place.getFullText(null).toString(),
+                            color = MaterialTheme.colorScheme.onPrimary,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(12.dp)
+                                .clickable {
+                                    fetchDetails(place.placeId) {
+                                        it.origin = LatLng(
+                                            uiState.searchResult.location?.latitude!!,
+                                            uiState.searchResult.location.longitude,
+                                        )
+                                        goToGoogleMap.invoke(it)
+                                        navHostController.navigate(LocationRoute.GOOGLE_MAPS.route)
+                                    }
+                                }
+                        )
+                    }
+                }
+            }
+
+            is PlacesResult.Error -> {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = uiState.searchResult.message.toString(),
+                        color = MaterialTheme.colorScheme.onError,
+                        modifier = Modifier.padding(24.dp)
+                    )
+                }
+            }
         }
+    }
+}
 
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun PermissionHandler(
+    navController: NavHostController,
+    permissionList: List<String> = listOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+) {
+    val permissionState = rememberMultiplePermissionsState(permissions = permissionList)
 
-    } else {
-        LaunchedEffect(key1 = Unit, block = {
-            permission.launchMultiplePermissionRequest()
-        })
+    LaunchedEffect(Unit) {
+        if (!permissionState.allPermissionsGranted) {
+            navController.navigate(DashboardRoute.PERMISSION_SCREEN.route)
+        }
+    }
+}
+
+@Preview(
+    name = "Light Mode",
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_NO
+)
+@Preview(
+    name = "Dark Mode",
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_YES
+)
+@Composable
+fun PlaceSearchPreview() {
+
+    val uiState = PlacesContract.PlacesUiState(
+        isLoading = false,
+        searchQuery = "",
+        searchResult = PlacesResult.Idle(),
+        error = null,
+    )
+    val context = LocalContext.current
+    BiteMapTheme {
+        ScreenUiContent(
+            uiState = uiState,
+            navHostController = NavHostController(context = context),
+            fetchDetails = { _, _ ->},
+            updateQuery = {},
+            goToGoogleMap = {}
+        )
     }
 }
--- app/src/main/java/novalogics/android/bitemap/location/presentation/screens/places/PlacesSearchViewModel.kt
+++ app/src/main/java/novalogics/android/bitemap/location/presentation/screens/places/PlacesSearchViewModel.kt
@@ -1,18 +1,14 @@
 package novalogics.android.bitemap.location.presentation.screens.places
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
-import novalogics.android.bitemap.core.navigation.events.PlacesResult
+import novalogics.android.bitemap.core.base.BaseViewModel
 import novalogics.android.bitemap.location.domain.model.PlaceDetails
 import novalogics.android.bitemap.location.domain.usecase.FetchRestaurantDetailUseCase
 import novalogics.android.bitemap.location.domain.usecase.SearchRestaurantUseCase
@@ -23,37 +19,46 @@ import javax.inject.Inject
 class PlacesSearchViewModel @Inject constructor(
     private val searchRestaurantUseCase: SearchRestaurantUseCase,
     private val fetchRestaurantDetailUseCase: FetchRestaurantDetailUseCase,
-): ViewModel() {
+) : BaseViewModel<PlacesContract.Intent, PlacesContract.PlacesUiState, PlacesContract.Effect>(
+    PlacesContract.PlacesUiState()
+) {
 
-    private val _search: MutableState<PlacesResult> = mutableStateOf<PlacesResult>(
-        PlacesResult.Idle()
-    )
-    val search: State<PlacesResult> get() = _search
-
-    private val _query: MutableStateFlow<String> = MutableStateFlow("")
-    val query: StateFlow<String> get() = _query
+    override fun handleIntent(intent: PlacesContract.Intent) {
+        when (intent) {
+            is PlacesContract.Intent.UpdateQuery -> updateQuery(intent.query)
+            is PlacesContract.Intent.SearchRestaurants -> searchRestaurants(intent.query)
+            is PlacesContract.Intent.FetchDetails -> fetchDetails(intent.placeId, intent.result)
+        }
+    }
 
     init {
-        viewModelScope.launch{
-            _query.debounce(1000).collectLatest {query->
-                searchRestaurants(query)
+        viewModelScope.launch {
+            uiState
+                .map { it.searchQuery }
+                .distinctUntilChanged()
+                .debounce(1000)
+                .collectLatest { query ->
+                    if (query.isNotEmpty()) {
+                        searchRestaurants(query)
+                    }
+                }
+        }
+    }
+
+    private fun updateQuery(query: String) {
+        updateState { copy(searchQuery = query) }
+    }
+
+    private fun searchRestaurants(query: String) = viewModelScope.launch {
+        searchRestaurantUseCase(query = query).collectLatest { places ->
+            updateState { copy(searchResult = places) }
+        }
+    }
+
+    private fun fetchDetails(placeId: String, result: (PlaceDetails) -> Unit) =
+        viewModelScope.launch {
+            fetchRestaurantDetailUseCase(placeId = placeId).collectLatest {
+                result.invoke(it)
             }
         }
-    }
-
-    fun updateQuery(query: String){
-        _query.value = query
-    }
-
-    fun searchRestaurants(query: String) = viewModelScope.launch {
-        searchRestaurantUseCase(query = query).collectLatest { places->
-            _search.value = places
-        }
-    }
-
-    fun fetchDetails(placeId:String, result: (PlaceDetails)-> Unit) = viewModelScope.launch {
-        fetchRestaurantDetailUseCase(placeId = placeId).collectLatest {
-            result.invoke(it)
-        }
-    }
 }
